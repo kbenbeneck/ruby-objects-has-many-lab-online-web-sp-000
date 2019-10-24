68b267239d59da11b9384f18102a2263fbeb629b
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-24 02:31:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,4 +3,17 @@ class Artist
    def initialize(name)
      @name = name
    end
+
+   def add_song(song)
+     song.artist = self
+   end
+
+   def songs
+     Song.all.select {|song| song.artist == self}
+   end
+
+   def add_song_by_name(name)
+     song = Song.new(name)
+     add_song(song)
+   end
 end
